6869d5820cb76d431f176e098d803b689886b159
Pablo Mérida <pablomerida2001@gmail.com>
2021-11-25 13:51:27 +0100

    header

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
     require_once "./model/User.php";
     require_once "./model/Game.php";
     require_once "./controller/db/query.php";
@@ -16,8 +15,6 @@
     $games = Game::getAllGames();
 
     $pag = $_GET["p"]??1 ;
-
-	//$total = ceil(Serie::totalShows()/MAX_ITEMS_PAGINA) ;
 ?>
 
 <!DOCTYPE html>
@@ -35,64 +32,7 @@
     <title>Inicio</title>
 </head>
 
-<header>
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <!-- Container wrapper -->
-        <div class="container-fluid">
-            <!-- Collapsible wrapper -->
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Navbar brand -->
-            <a class="navbar-brand mt-2 mt-lg-0" href="#">
-                <img src="https://mdbootstrap.com/img/logo/mdb-transaprent-noshadows.png" height="15" alt="logo" loading="lazy"/>
-            </a>
-            <!-- Left links -->
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Inicio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Test</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Test</a>
-                </li>
-            </ul>
-            <!-- Left links -->
-            </div>
-            <!-- Collapsible wrapper -->
-
-            <!-- Right elements -->
-            <div class="d-flex align-items-center">
-            <!-- Icon -->
-            <a class="text-reset me-3" href="#">
-                <i class="fas fa-shopping-cart"></i>
-            </a>
-            <?php if($user != null){ ?>
-                <div class="dropdown">
-                    <a class="dropdown-toggle text-white" data-toggle="dropdown"><?= $user->__get("userName")?></a>
-
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-                        <a class="dropdown-item text-danger" href="Controller/Login/Logout.php">Cerrar Sesión</a>
-                    </div>
-                </div>
-            <?php }else{ ?>
-
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
-                <li class="nav-item px-2">
-                    <a type="button" name="login" id="loginButton" class="btn btn-success" href="View/Login.php">Iniciar Sesión</a>  
-                </li>
-                <li class="nav-item px-2">
-                    <a type="button" name="register" id="registerButton" class="btn btn-primary" href="View/register.php">Registrate</a>  
-                </li>
-            </ul>
-            <?php } ?>
-            <!-- Right elements -->
-        </div>
-        <!-- Container wrapper -->
-    </nav>
-    <!-- Navbar -->
-</header>
+<?php require_once "View/Header.php"?>
 
 <body>
 
